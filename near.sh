57596072a1b4b43229c9f7fe8091ccfda5b34ccf
Aomine <161364808+AomineAether@users.noreply.github.com>
2024-03-24 21:57:31 +0700

    Update near.sh

--- near.sh
+++ near.sh
@@ -4,7 +4,7 @@ RPC_URL="https://near.lava.build/lava-referer-8eebd84a-8b46-4cd1-ae96-03e61d1c3a
 
 OUTPUT_DIR="logs"
 
-near_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"block","id":"HappyCuanAirdrop","params":{"finality":"final"}}' "$RPC_URL")
+near_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"block","id":"Aethereal","params":{"finality":"final"}}' "$RPC_URL")
 
 if [ $? -eq 0 ]; then
     if [ ! -d "$OUTPUT_DIR" ]; then
